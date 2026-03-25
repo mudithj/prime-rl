1c192cc04384e0d1e2271993b564bdcb2b8ab8e5
S1ro1 <matej.sirovatka@gmail.com>
2026-03-25 22:27:41 +0530

    Fix style

--- src/prime_rl/inference/patches.py
+++ src/prime_rl/inference/patches.py
@@ -740,13 +740,8 @@ def monkey_patch_dp_engine_core_pause_resume_deadlock():
         if self.has_coordinator and request_wave != self.current_wave:
             if request_wave > self.current_wave:
                 self.current_wave = request_wave
-            elif (
-                not self.engines_running
-                and self.scheduler.pause_state == PauseState.UNPAUSED
-            ):
+            elif not self.engines_running and self.scheduler.pause_state == PauseState.UNPAUSED:
                 self.engines_running = True
-                self.output_queue.put_nowait(
-                    (-1, EngineCoreOutputs(start_wave=self.current_wave))
-                )
+                self.output_queue.put_nowait((-1, EngineCoreOutputs(start_wave=self.current_wave)))
 
     DPEngineCoreProc.add_request = _patched_add_request
